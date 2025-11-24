6440f2c5b88a0716a72a558a5ef3ceda6f3ac9d5
mmodzelewski23 <m.modzelewski23@zsek.olsztyn.pl>
2025-11-24 20:23:05 +0100

    Dodanie zapisywanie listy w pamięci
    Dodanie funkcji zapisywania listy w pamięci przeglądarki

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const taskForm = document.getElementById('task-form');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
+    var zadania = [];
+    var index;
     taskForm.addEventListener('submit', function (e) {
-
         e.preventDefault();
         addTask(taskInput.value);
+        // aktualizacja pamieci
+        zadania.push(taskInput.value);
+        console.log(zadania);
+        localStorage.setItem('zadania', JSON.stringify(zadania));
         taskInput.value = '';
     });
     function addTask(taskText) {
@@ -19,13 +24,52 @@ document.addEventListener('DOMContentLoaded', function () {
 </div>
 `;
         taskList.appendChild(li);
+        return li;
     }
     window.toggleComplete = function (btn) {
         const taskItem = btn.closest('.task-item');
+        let taskItemText = taskItem.querySelector('.task-text').textContent;
+        // aktualizacja pamieci
+        console.log("Content: "+taskItemText);
+        if (taskItem.classList.contains('completed')) {
+            taskItemText="%done%"+taskItemText; 
+        }
+        index = zadania.indexOf(taskItemText);
+        console.log(index+'<- index zadania[index] = '+zadania[index]);
+        if (zadania[index].includes("%done%")) {
+            zadania[index] = zadania[index].replace('%done%', '');
+        } else {
+            zadania[index] = "%done%" + zadania[index];
+        }
+        localStorage.setItem('zadania', JSON.stringify(zadania));
+        
         taskItem.classList.toggle('completed');
     }
     window.deleteTask = function (btn) {
         const taskItem = btn.closest('.task-item');
+        // aktualizacja pamięci
+        let taskItemText = taskItem.querySelector('.task-text').textContent;
+        if (taskItem.classList.contains('completed')) {
+            taskItemText="%done%"+taskItemText;
+        }
+        index = zadania.indexOf(taskItemText);
+        zadania.splice(index, 1);
+        localStorage.setItem('zadania', JSON.stringify(zadania));
+
         taskItem.remove();
+    }
+
+    let cookieZadania = localStorage.getItem('zadania');
+    console.log(cookieZadania);
+    if (cookieZadania) {
+        cookieZadania = JSON.parse(cookieZadania);
+        cookieZadania.forEach(e => {
+            if (e.startsWith('%done%')) {
+                addTask(e.replace('%done%', '')).classList.toggle('completed');;
+            } else {
+                addTask(e);
+            }
+        });
+        zadania=cookieZadania;
     }
 });
